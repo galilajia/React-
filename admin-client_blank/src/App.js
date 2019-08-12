66d050beb7e579c87d0e573eade064c628b46898
chenguanan <754964515@qq.com>
2019-08-12 18:54:09 +0800

    login.jsx

--- admin-client_blank/src/App.js
+++ admin-client_blank/src/App.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react'
-import {Route,Switch,HashRouter,BrowserRouter} from 'react-router-dom'
+import {Route,Switch,BrowserRouter} from 'react-router-dom'
 import Admin from './page/admin/admin'
 import Login from './page/login/login'
 
 export default class App extends Component {
   render() {
     return (
-      <div>
+     
         <BrowserRouter>
         <Switch>
           <Route path = '/login' component={Login} />
           <Route path = '/admin' component={Admin} />
         </Switch>
-         
         </BrowserRouter>
-      </div>
+     
     )
   }
 }
--- admin-client_blank/src/page/login/login.jsx
+++ admin-client_blank/src/page/login/login.jsx
@@ -1,11 +1,73 @@
 import React, { Component } from 'react'
+import { Form, Icon, Input, Button } from 'antd';
 
-export default class Login extends Component {
+import './login.less'
+import logo from './images/logo.png'
+
+const Item = Form.Item
+ class Login extends Component {
+    handleSubmit = e => {
+    
+        e.preventDefault()
+    
+        const values = this.props.form.getFieldsValue()
+        const username = this.props.form.getFieldValue('username')
+        const pwd = this.props.form.getFieldValue('password')
+        console.log(values, username, pwd)
+    
+        alert('发送登陆的ajax请求')
+      }
     render() {
+        //获取
+        const {getFieldDecorator} = this.props.form 
+        console.log(this)       
         return (
-            <div>
-                Login
+            <div className = 'login'>
+                <div className = 'login-header'>
+                     <img src={logo} alt="logo"/>
+                     <h1>后台管理系统</h1>
+                </div>
+                <div className="login-content">
+          <h1>用户登陆</h1>
+          <Form onSubmit={this.handleSubmit} className="login-form">
+            <Item>
+
+              {
+                getFieldDecorator('username', { 
+                })(
+                  <Input
+                    prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                    placeholder="用户名"
+                  />
+                )
+              }
+              
+            </Item>
+            <Form.Item>
+              {
+                getFieldDecorator('password', {
+
+                })(
+                  <Input
+                    prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                    type="password"
+                    placeholder="密码"
+                  />
+                )
+              }
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit" className="login-form-button">
+                登  陆
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
             </div>
         )
     }
 }
+
+const WrappedLoginForm = Form.create()(Login)  
+
+export default WrappedLoginForm
