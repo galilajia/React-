f6f3f544697bd10edd9d524e42a020ad8afc85f7
chenguanan <754964515@qq.com>
2019-08-14 08:12:19 +0800

    admin

--- admin-client_blank/src/page/admin/admin.jsx
+++ admin-client_blank/src/page/admin/admin.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
-import {Redirect} from 'react-router-dom'
-import memoryUtils from'../../utils/memoryUtils'
+import { Redirect } from 'react-router-dom'
+import memoryUtils from '../../utils/memoryUtils'
 export default class Admin extends Component {
     render() {
         const user = memoryUtils.user
         //如果用户没登陆自动跳转登陆界面
         console.log(user)
-        if(!user._id){
+        if (!user._id) {
             //在render中跳转用 Redirect
-            return <Redirect to = '/login'/>
+            return <Redirect to='/login' />
             //在事件回调函数中跳转用history.replace()
         }
 
